d466de95e71a1b40fdea9d7992c18db5ca015211
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-01-14 20:09:38 +0300

    add home page component

---
+++ src/components/__tests__/HomePage.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils';
+import HomePage from './../HomePage'; // eslint-disable-line
+
+describe('HomePage', () => {
+  it('should render', () => {
+    const wrapper = shallowMount(HomePage);
+    expect(wrapper.find('h2').text()).toEqual('Industrial Attachment Application System');
+  });
+});
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+
+import BootstrapVue from 'bootstrap-vue';
+
 import App from './App.vue';
 
+import 'bootstrap/dist/css/bootstrap.css'; // eslint-disable-line import/no-extraneous-dependencies
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
