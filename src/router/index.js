aa5e0ccbc5d7c0ece7707fe1275f4bade36771f7
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-01-24 17:11:51 +0300

    design registration form

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import HomePage from '../components/Pages/HomePage/HomePage.vue';
-import SignupPage from '../components/Pages/SignupPage/SignupPage.vue';
+import HomePage from '../views/Home/HomePage.vue';
+import SignupPage from '../views/Signup/SignupPage.vue';
 
 
 Vue.use(Router);
---
+++ src/views/Signup/SignupPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SignupForm/>
+  </div>
+</template>
+
+<script>
+import SignupForm from '../../components/Signup/SignupForm.vue';
+
+export default {
+  name: 'SignupPage',
+  components: {
+    SignupForm,
+  },
+};
+</script>
