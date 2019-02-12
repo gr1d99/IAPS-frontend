1d98d76bf103a96bf53064156bbb35902b4924bb
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-02-12 09:44:55 +0300

    axios modifications

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import axios from 'axios';
 import VueAxios from 'vue-axios';
+import axios from '../services';
 
 Vue.use(VueAxios, axios);
---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'allow';
+
+export default axios;
