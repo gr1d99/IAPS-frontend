5c9c134bf953c0e06d2a1cdae3a6e93828bffa6d
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-02-26 22:17:15 +0300

    ensure notification message is cleared

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -1,28 +1,4 @@
-import Users from '../../../services/users';
-import { BAD_REQUEST_MESSAGE } from '../../../constants/messages';
-import { DONE_TYPE, WAITING_TYPE } from '../../../constants/async_types';
-
 const actions = {
-  createUser: ({ commit }, data) => {
-    commit('setLoading', WAITING_TYPE);
-    Users.create(data).then((response) => {
-      const accessToken = response.headers['x-access-token'];
-      window.$cookies.set('jwt-token', accessToken);
-      commit('userCreated');
-    }).catch((error) => {
-      commit('setLoading', DONE_TYPE);
-      switch (error.response.status) {
-        case 400:
-          commit('addErrors', [BAD_REQUEST_MESSAGE]);
-          break;
-        case 422:
-          commit('addValidationErrors', error.response.data.errors);
-          break;
-        default:
-          break;
-      }
-    });
-  },
   resetErrors: ({ commit }) => {
     commit('resetErrors');
   },
--- src/views/Users/Create/mixins/createUserMixin.js
+++ src/views/Users/Create/mixins/createUserMixin.js
@@ -15,11 +15,15 @@ const mixin = {
       };
       this.$store.commit('users/setLoading', WAITING_TYPE);
       Users.create(data).then((response) => {
+        this.$store.commit('users/setLoading', DONE_TYPE);
         const accessToken = response.headers['x-access-token'];
         this.$cookies.set('jwt-token', accessToken);
         this.$store.commit('users/userCreated');
         this.$root.$emit('user-logged-in');
         this.$router.push('/');
+        setTimeout(() => {
+          this.$store.commit('users/resetUserCreated');
+        }, process.env.VUE_APP_CLEAR_NOTIFICATION_MESSAGE_AFTER || 3000);
       }).catch((error) => {
         this.$store.commit('users/setLoading', DONE_TYPE);
         switch (error.response.status) {
