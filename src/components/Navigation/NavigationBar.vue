c492f15cbadaf41074745591b6313376a1b13e43
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-04-24 13:07:46 +0300

    integrate modern business template

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -1,48 +1,57 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <router-link class="navbar-brand" :to="{ name: 'Home' }">IAAS</router-link>
-    <button class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#iaasCollapse"
-            aria-controls="iaasCollapse"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="iaasCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <router-link
-            class="nav-link"
-            :to="{ name: 'Home' }">Home <span class="sr-only">(current)</span></router-link>
-        </li>
-        <li class="nav-item" v-if="this.isLoggedIn && this.isAdmin">
-          <router-link :to="{ name: 'CreateOpening' }" class="nav-link">Create Opening</router-link>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item" v-if="this.isLoggedIn">
-          <a class="nav-link"
-             href="#"
-             @click="logoutUser">Logout</a>
-        </li>
-
-        <!--# TODO --start-- move this to its own component-->
-        <li class="nav-item" v-if="!this.isLoggedIn">
-          <router-link
-            class="nav-link"
-            :to="{ name: 'CreateUser' }">Signup</router-link>
-        </li>
-
-        <li class="nav-item" v-if="!this.isLoggedIn">
-          <router-link
-            class="nav-link"
-            :to="{ name: 'CreateSession' }">Login</router-link>
-        </li>
-        <!--# TODO --end-- -->
-      </ul>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">IAAS</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services.html">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Portfolio
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+              <a class="dropdown-item" href="portfolio-1-col.html">1 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-2-col.html">2 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-3-col.html">3 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+              <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Blog
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+              <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+              <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Other Pages
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+              <a class="dropdown-item" href="full-width.html">Full Width Page</a>
+              <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
+              <a class="dropdown-item" href="faq.html">FAQ</a>
+              <a class="dropdown-item" href="404.html">404</a>
+              <a class="dropdown-item" href="pricing.html">Pricing Table</a>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -50,17 +59,7 @@
 <script>
 export default {
   name: 'NavigationBar',
-  props: ['isLoggedIn', 'isAdmin'],
-  data() { return {}; },
-  methods: {
-    logoutUser() {
-      this.$cookies.remove('jwt-token');
-      this.$root.$emit('user-logged-out');
-    },
-  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Shared/PageFooter.vue
@@ -0,0 +1,18 @@
+<template>
+<footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+  </div>
+  <!-- /.container -->
+</footer>
+</template>
+
+<script>
+export default {
+  name: 'PageFooter',
+};
+</script>
+
+<style scoped>
+
+</style>
