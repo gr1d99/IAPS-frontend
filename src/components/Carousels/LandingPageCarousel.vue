c735b88ba10846957dab668cad87577ee35b62ba
Gideon Kimutai <gideonkimutai9@gmail.com>
2019-05-16 20:36:43 +0300

    move carousel to its own component

---
+++ src/components/Carousels/LandingPageCarousel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="carousel-item active" :style="{'background-image': backgroundImageUrl}">
+    <div class="carousel-caption d-none d-md-block">
+      <h3>{{ title }}</h3>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPageCarousel',
+  props: ['backgroundImageUrl', 'title', 'description'],
+};
+</script>
+
+<style scoped>
+
+</style>
